e743675df11cc1e12f420209e27c3bc6a3535a1a
Rishi <rishi.helloworld@gmail.com>
2024-05-08 17:51:11 +0530

    hello world

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+app.get('/path_1', (req, res) => {
+    res.send('Hello World in Path 1');
+  });
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
